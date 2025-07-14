4133402352dc3703a92edeb89fab1dee3dc531a4
clementine iradukunda <clementineiradu365@gmail.com>
2025-07-14 10:32:56 +0200

    all

--- server.js
+++ server.js
@@ -6,6 +6,8 @@
 /* ***********************
  * Require Statements
  *************************/
+require("dotenv").config();
+
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const session = require("express-session")
